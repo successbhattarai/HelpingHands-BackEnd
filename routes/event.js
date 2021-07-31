d363e7a895b3559db4b19e551297a6923d2994c2
Success Bhattarai <bhabisbhai@gmail.com>
2021-07-31 08:30:07 +0545

    Event Category Wise Routes Changed

--- routes/event.js
+++ routes/event.js
@@ -74,14 +74,6 @@ router.get('/event/display',(req,res) => {
 	})
 });
 
-router.get('/event/display/education',(req,res) => {
-	var education = { eventCategories: "Education" };
-	Event.find(education)
-	.then(function(Post){
-		res.send(Post);
-	})
-});
-
 router.get('/event/display/limit=3',(req,res) => {
 	Event.find().limit(3)
 	.then(function(Post){
@@ -99,7 +91,6 @@ router.get("/my-event/display/:id",function(req,res){
 	})
 });
 
-// Event Display Single
 router.get("/event/display/:id",function(req,res){    
     const id = req.params.id;
     Event.findOne({_id:id})
@@ -110,5 +101,80 @@ router.get("/event/display/:id",function(req,res){
         res.status(500).json({message : err})
     })
 });
+
+// Event Category Wise
+// Event Category : Entertainment
+router.get('/event/display/entertainment',(req,res) => {
+	var entertainment = { eventCategories: "Entertainment" };
+	Event.find(entertainment)
+	.then(function(Post){
+		res.send(Post);
+	})
+});
+
+// Event Category : Social
+router.get('/event/display/social',(req,res) => {
+	var social = { eventCategories: "Social" };
+	Event.find(social)
+	.then(function(Post){
+		res.send(Post);
+	})
+});
+
+// Event Category : Sports
+router.get('/event/display/sports',(req,res) => {
+	var sports = { eventCategories: "Sports" };
+	Event.find(sports)
+	.then(function(Post){
+		res.send(Post);
+	})
+});
+
+// Event Category : Religious
+router.get('/event/display/religious',(req,res) => {
+	var religious = { eventCategories: "Religious" };
+	Event.find(religious)
+	.then(function(Post){
+		res.send(Post);
+	})
+});
+
+// Event Category : Educational
+router.get('/event/display/educational',(req,res) => {
+	var educational = { eventCategories: "Educational" };
+	Event.find(educational)
+	.then(function(Post){
+		res.send(Post);
+	})
+});
+
+// Event Category : Political
+router.get('/event/display/political',(req,res) => {
+	var political = { eventCategories: "Political" };
+	Event.find(political)
+	.then(function(Post){
+		res.send(Post);
+	})
+});
+
+// Event Category : Charitable
+router.get('/event/display/charitable',(req,res) => {
+	var charitable = { eventCategories: "Charitable" };
+	Event.find(charitable)
+	.then(function(Post){
+		res.send(Post);
+	})
+});
+
+// Event Category : Others
+router.get('/event/display/others',(req,res) => {
+	var others = { eventCategories: "Others" };
+	Event.find(others)
+	.then(function(Post){
+		res.send(Post);
+	})
+});
+
+
 
 module.exports = router;
