06aac637d8538264d9a98135d117a487c5ebf1ce
sabinbudhathoki07 <50662122+sabinbudhathoki07@users.noreply.github.com>
2021-08-27 10:21:46 +0545

    nodemodules have been changed

--- app.js
+++ app.js
@@ -38,11 +38,11 @@ app.use(laterRoutes);
 app.use(bodyParser.urlencoded({extended:false}));
 
 // enable CORS without external module
-app.use(function (req, res, next) {
-     res.header("Access-Control-Allow-Origin", "*");
-     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-     next();
-});
+// app.use(function (req, res, next) {
+//      res.header("Access-Control-Allow-Origin", "*");
+//      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//      next();
+// });
 
 app.get("/", (req, res)=>{
      res.send("Welcome to helping hands");
